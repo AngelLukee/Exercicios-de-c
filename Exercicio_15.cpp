c40f3f12787088d80f0632aedbf95af8e34f6ab7
Luuh <111019855+AngelLukee@users.noreply.github.com>
2024-05-15 00:22:56 -0300

    Rename Exercicio 15.cpp to Exercicio_15.cpp

--- Exercicio_15.cpp
+++ Exercicio_15.cpp
@@ -6,7 +6,7 @@ int main(){
 	int y;
 	float z;
 	
-	printf("escreva UM caractere, UM numero inteiro e UM numero decimal, espaçados entre eles\n");
+	printf("escreva UM caractere, UM numero inteiro e UM numero decimal, espaÃ§ados entre eles\n");
     scanf("%c%d%f", &x, &y, &z);
 	printf("separadas por espaco-> %c %d %.1f \nseparados por tab->\t%c\t%d\t%.1f \nou separados por linha-> \n%c\n%d\n%.1f\n", x,y,z,x,y,z,x,y,z);	
 }
